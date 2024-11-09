e7be011fc483f2a18b3ce8159e76996a0426e990
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-09 18:52:37 -0500

    New lot endpoints

--- backend/park_smart_analytics/views.py
+++ backend/park_smart_analytics/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Lots
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserRegisterSerializer, UserLoginSerializer
+from .serializers import (
+    UserRegisterSerializer,
+    UserLoginSerializer,
+    LotSerializer,
+    LotsSerializer,
+)
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -20,9 +27,21 @@ class RegisterView(generics.CreateAPIView):
 
 
 class LoginView(APIView):
-    serializer_class = UserLoginSerializer
-
-    def post(self, request, *args, **kwargs) -> Response:
-        serializer = self.serializer_class(data=request.data)
+    def post(self, request) -> Response:
+        serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class LotView(APIView):
+    def get(self, request, lot_id) -> Response:
+        lot = get_object_or_404(Lots, lot_id=lot_id)
+        serializer = LotSerializer(lot)
+        return Response(serializer.data)
+
+
+class LotsView(APIView):
+    def get(self, request) -> Response:
+        lots = Lots.objects.all()
+        serializer = LotsSerializer(lots, many=True)
+        return Response(serializer.data)
