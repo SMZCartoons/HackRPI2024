cdaa74cfad2f7c5982e96c773092cf69df871a36
Josh Youngbar <54415235+jyoungbar@users.noreply.github.com>
2024-11-09 23:34:57 -0500

    combined home page

--- frontend/src/components/lotStats.tsx
+++ frontend/src/components/lotStats.tsx
@@ -1,27 +1,53 @@
-import { Row, Col, Container } from "react-bootstrap";
-import { useState } from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import { useState, useRef } from "react";
 
-function LotStats() {
-  const [totalSpots, setTotalSpots] = useState("-");
-  const [capacity, setCapacity] = useState("-");
-  const [spotsAvailable, setSpotsAvailable] = useState("-");
+function LotStats(LotInfo : any) {
+  // const [totalSpots, setTotalSpots] = useState(LotInfo.totalSpots);
+  // const [capacity, setCapacity] = useState("-");
+  // const [spotsAvailable, setSpotsAvailable] = useState("-");
 
+  // const ref = useRef(null);
+
+  const infoButtonClick = (e : any) => {
+    e.preventDefault();
+    console.log(LotInfo)
+
+    //scrollintoview
+    var element = document.getElementById("bottom-row");
+    element?.scrollIntoView({behavior: 'smooth'});
+  }
 
   return (
     <>
       <Container fluid className="lotStatsContainer">
+        <div className="info-button-div"><Button variant="info" onClick={(e) => infoButtonClick(e)}>Lot Info</Button></div>
         <Row>
           <Col>
             <h5>Total Spots</h5>
-            <h2>{totalSpots}</h2>
+            <h2>{LotInfo.totalSpots}</h2>
           </Col>
           <Col>
             <h5>Capacity</h5>
-            <h2>{capacity}</h2>
+            <h2>{LotInfo.capacity}</h2>
           </Col>
           <Col>
             <h5>Spots Available</h5>
-            <h2>{spotsAvailable}</h2>
+            <h2>{LotInfo.spotsAvailable}</h2>
+          </Col>
+        </Row>
+
+        <Row id="bottom-row">
+          <Col>
+            <h5>Chargers Available</h5>
+            <h2>{LotInfo.chargersAvailable}</h2>
+          </Col>
+          <Col>
+            <h5>Handicap Spots Available</h5>
+            <h2>{LotInfo.handicap}</h2>
+          </Col>
+          <Col>
+            <h5>Parking Lot Access</h5>
+            <h2>{LotInfo.lotAccess}</h2>
           </Col>
         </Row>
       </Container>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,19 @@
 import LotStats from "../components/lotStats";
 import ParkingMap from "../components/map";
+import { useState } from "react";
 
 function Home() {
+  // const [lotData, setLotData] = useState<any>({totalSpots: "-", capacity: "-", spotsAvailable: "-", chargersAvailable: "-", handicap: "-", lotAccess: "-"});
+
+  // const handleClick = () => {
+  //   setLotData({totalSpots: "-", capacity: "-", spotsAvailable: "oejfi", chargersAvailable: "-", handicap: "-", lotAccess: "-"});
+  //   console.log(lotData);
+  // }
   return (
     <div className="page">
       <div className="home-page">
           <ParkingMap />
-          <LotStats />
+          {/* <LotStats LotInfo={lotData}/> */}
       </div>
     </div>
   );
