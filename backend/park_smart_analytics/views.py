cd894f9a02e04546e1a458b9608149f88d7f6942
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2024-11-10 03:32:50 -0500

    Add new routes for check in and check out

--- backend/park_smart_analytics/views.py
+++ backend/park_smart_analytics/views.py
@@ -1,16 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from .models import Lots
 from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .models import Lots
 from .serializers import (
-    UserRegisterSerializer,
-    UserLoginSerializer,
+    LeaderBoardSerializer,
     LotSerializer,
     LotsSerializer,
+    UserLoginSerializer,
+    UserRegisterSerializer,
 )
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -45,3 +49,86 @@ class LotsView(APIView):
         lots = Lots.objects.all()
         serializer = LotsSerializer(lots, many=True)
         return Response(serializer.data)
+
+
+class CheckIn(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, lot_id) -> Response:
+        data = request.data  # This is already a parsed dictionary
+        is_electric = False
+        is_disability = False
+        if data:
+            is_disability = data.get("disability", False)
+            is_electric = data.get("electric", False)
+
+        lot = get_object_or_404(Lots, lot_id=lot_id)
+        user = request.user
+
+        with transaction.atomic():
+            lot.available -= 1
+            user.checked_in = True
+
+            if is_electric:
+                lot.electrified_available -= 1
+                user.checked_in_electric = True
+            if is_disability:
+                lot.handicap_available -= 1
+                user.checked_in_disability = True
+
+            user.points += 1
+
+            lot.save()
+            user.save()
+
+        return Response({"status": "ok"})
+
+
+class CheckOut(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, lot_id) -> Response:
+
+        lot = get_object_or_404(Lots, lot_id=lot_id)
+        user = request.user
+
+        with transaction.atomic():
+            lot.available += 1
+            user.checked_in = False
+
+            if user.checked_in_electric:
+                lot.electrified_available += 1
+                user.checked_in_electric = False
+            if user.checked_in_disability:
+                lot.handicap_available += 1
+                user.checked_in_disability = False
+
+            user.points += 2
+
+            lot.save()
+            user.save()
+
+        return Response({"status": "ok"})
+
+
+class LeaderBoard(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request) -> Response:
+        users = User.objects.all().order_by("-points")[:20]
+        user = request.user
+
+        serializer = LeaderBoardSerializer(
+            users, many=True, context={"queryset": users}
+        )
+
+        serialized_data = list(serializer.data)
+
+        if user not in users:
+            user_rank = users.filter(points__gt=user.points).count() + 1
+            current_user_data = LeaderBoardSerializer(
+                user, context={"rank": user_rank}
+            ).data
+            serialized_data.append(current_user_data)
+
+        return Response(serialized_data)
